3db3306dfeb36387cbd66f61a11dbf527be1b480
Anbuvel-2003 <anbusmartanbuvel2407@gmail.com>
2025-06-27 16:17:16 +0530

    basic setup

---
+++ src/pages/splash.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Flex, Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+function Splash() {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-r from-[#E770C0] via-[#C670DE] to-[#9F70FD]  grid items-center justify-center">
+      <div>
+        <Spin
+          indicator={
+            <LoadingOutlined style={{ fontSize: 100, color: "#fff" }} spin />
+          }
+        />
+        <h4 className="text-white text-[50px] capitalize font-monoton " >accrix</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Splash;
